b0e5f8c810c82fb9c46ccb48a8871028240d57a8
lajawanti <lajawanti.dhake@gmail.com>
2020-02-05 23:53:56 -0500

    insert_node at start

--- linked_list.py
+++ linked_list.py
@@ -5,9 +5,27 @@ class Node:
         self.next = None
 
 #Using above Node object construct Linked list
-def Linkedlist:
-    # linked list will have head 
+class Linkedlist:
+    # linked list will have head by default so its in __init__
+    # if list empty self.head = null 
     def __init__(self):
         self.head = None
+
+    # We can insert a node in linked list in three ways
+    # 1. Insert at start O(1)
+    # 2. Insert at some position
+    # 3. Inset at the end O(n)
+    def insert_at_start(data):
+        new_node_to_insert = Node(data)
+        if self.head == None:
+            self.head = new_node_to_insert
+        else: 
+            new_node_to_insert.next = head
+            self.head = new_node_to_insert
+            
+        return self.head
+    
+    
+
     
     
