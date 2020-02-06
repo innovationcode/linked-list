98bf1006ad79b433c9212a5d92f2de883ae305ab
lajawanti <lajawanti.dhake@gmail.com>
2020-02-05 23:56:59 -0500

    insert_node at some position

--- linked_list.py
+++ linked_list.py
@@ -16,16 +16,40 @@ class Linkedlist:
     # 2. Insert at some position
     # 3. Inset at the end O(n)
     def insert_at_start(data):
-        new_node_to_insert = Node(data)
+        node_to_insert = Node(data)
         if self.head == None:
-            self.head = new_node_to_insert
+            self.head = node_to_insert
         else: 
-            new_node_to_insert.next = head
-            self.head = new_node_to_insert
+            node_to_insert.next = self.head
+            self.head = node_to_insert
             
         return self.head
     
+    def insert_in_between(self, data, position):
+        self.node_position = 0 
+        node_to_insert = Node(data)
+        if self.head is None:
+            self.head = node_to_insert
+            #self.node_position += 1
+            return self.head
+            
+        elif self.head is not None:
+            current = self.head
+            while current.next:
+                if (self.node_position + 1) == position:
+                    break
+                else:
+                    current = current.next
+                    self.node_position += 1 
+            
+        store_next = current.next
+        current.next = node_to_insert
+        node_to_insert.next = store_next
+        
+        return self.head
+
     
+
 
     
     
